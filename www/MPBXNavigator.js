ee565433cb7e861c1658520cfa7ab45cc199a4a9
Vahn84 <vahn@Fabios-MBP.fritz.box>
2017-06-12 17:05:29 +0200

    Edited js

--- www/MPBXNavigator.js
+++ www/MPBXNavigator.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function(arg0, success, error) {
+exports.showMap = function(arg0, success, error) {
     exec(success, error, "MPBXNavigator", "coolMethod", [arg0]);
 };
